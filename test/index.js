6b76972656d9d03e5e2610adc50eb97cc086cdfc
sajera <allsajera@gmail.com>
2018-02-07 21:08:32 +0200

    * fix linter warning

--- test/index.js
+++ test/index.js
@@ -31,28 +31,43 @@ describe('MVC Core', function () {
         expect(Core).to.be.a('function');
     });
 
-    it('has static classes', function () {
-        expect(Core.ErrorBase, "ErrorBase").to.be.a('function');
-        expect(Core.ErrorSpec, "ErrorSpec").to.be.a('function');
-        expect(Core.ModuleBase, "ModuleBase").to.be.a('function');
-        expect(Core.Bootstrap, "ModuleBase").to.be.a('function');
-        // TODO connect others
-
+    it('should has @static class "Logger"', function () {
+        expect(Core.Logger).to.be.a('function');
     });
 
-    it('has static methods', function () {
-        expect(Core.create, "create").to.be.a('function');
+    it('should has @static class "Configuration"', function () {
+        expect(Core.Configuration).to.be.a('function');
+    });
+
+    it('should has @static class "ModuleBase"', function () {
+        expect(Core.ModuleBase).to.be.a('function');
+    });
+
+    it('should has @static class "Bootstrap"', function () {
+        expect(Core.Bootstrap).to.be.a('function');
+    });
+
+    it('should has @static class "ErrorBase"', function () {
+        expect(Core.ErrorBase).to.be.a('function');
+    });
+
+    it('should has @static class "ErrorSpec"', function () {
+        expect(Core.ErrorSpec).to.be.a('function');
+    });
+
+    it('has @static methods', function () {
+        expect(Core.create).to.be.a('function');
+        expect(Core.initialize).to.be.a('function');
         // TODO connect others
 
     });
 
     it('has static props', function () {
-        expect(Core.logger, "logger").to.be.a('object');
-        expect(Core.config, "config").to.be.a('object');
-        expect(Core.utils, "utils").to.be.a('object');
+        expect(Core.logger).to.be.a('object');
+        expect(Core.config).to.be.a('object');
+        expect(Core.utils).to.be.a('object');
         // TODO connect others
 
     });
-
 
 });
